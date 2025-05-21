c1a2c02a092d74329c41228bc9e47e99c6e7a3d2
erikstiven <erikquisnia@gmail.com>
2025-05-20 23:29:11 -0500

    Agregar validación y redirección en el método update del FamilyController

--- app/Http/Controllers/Admin/FamilyController.php
+++ app/Http/Controllers/Admin/FamilyController.php
@@ -54,6 +54,7 @@ class FamilyController extends Controller
     public function edit(Family $family)
     {
         //
+        return view ('admin.families.edit', compact('family'));
     }
 
     /**
@@ -62,6 +63,12 @@ class FamilyController extends Controller
     public function update(Request $request, Family $family)
     {
         //
+        $request ->validate([
+            'name' => 'required',
+        ]);
+        $family->update($request->all());
+
+        return redirect()->route('admin.families.edit',$family);
     }
 
     /**
