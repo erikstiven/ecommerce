ab7655f1af86555e9b8e02e2844879dab3a6a5fb
erikstiven <erikquisnia@gmail.com>
2025-06-20 17:05:08 -0500

    Agregar funcionalidad para la creación de productos: se implementa la carga de imágenes, validaciones y se actualizan las vistas para incluir subcategorías y familias.

--- app/Livewire/Admin/Products/ProductCreate.php
+++ app/Livewire/Admin/Products/ProductCreate.php
@@ -6,11 +6,19 @@ use App\Models\Family;
 use Livewire\Component;
 use Livewire\Attributes\Computed;
 use App\Models\Category;
+use App\Models\Product;
+use App\Models\Subcategory;
+use Livewire\WithFileUploads;
 
 class ProductCreate extends Component
 {
+    use WithFileUploads;
+
     public $families;
     public $family_id = '';
+    public $category_id = '';
+
+    public $image;
 
     public $product = [
         'sku' => '',
@@ -25,12 +33,69 @@ class ProductCreate extends Component
     {
         $this->families = Family::all();
     }
-    //propiedad computada
-    #[Computed()]
-    public function categories(){
-        return Category::where('family_id', $this->family_id)->get();
+
+    public function boot(){
+        $this->withValidator(function ($validator) {
+            if($validator->fails()){
+                $this->dispatch('swal', [
+                    'icon' => 'error',
+                    'title' => '¡Error!',
+                    'text' => 'Por favor, corrige los errores en el formulario.',
+                ]);
+            }
+        });
 
     }
+
+    public function updatedFamilyId($value)
+    {
+        $this->category_id = '';
+        $this->product['subcategory_id'] = '';
+    }
+
+    public function updatedCategoryId($value)
+    {
+        $this->product['subcategory_id'] = '';
+    }
+    //propiedad computada
+    #[Computed()]
+    public function categories()
+    {
+        return Category::where('family_id', $this->family_id)->get();
+    }
+
+    #[Computed()]
+    public function subcategories()
+    {
+        return Subcategory::where('category_id', $this->category_id)->get();
+    }
+
+    public function store()
+    {
+        $this->validate([
+            'image' => 'required|image|max:1024', // 1MB Max
+            'product.sku' => 'required|unique:products,sku',
+            'product.name' => 'required |max:255',
+            'product.description' => 'nullable',
+            'product.price' => 'required|numeric|min:0',
+            'product.subcategory_id' => 'required|exists:subcategories,id',
+        ]);
+
+        $this->product['image_path'] = $this->image->store('products');
+        $product = Product::create($this->product);
+
+        session()->flash('swal', [
+            'icon' => 'success',
+            'title' => '¡Bien hecho!',
+            'text' => 'El producto ha sido creado exitosamente.',
+        ]
+            
+        );
+        return redirect()->route('admin.products.edit', $product);
+    }
+
+
+
     public function render()
     {
         return view('livewire.admin.products.product-create');
