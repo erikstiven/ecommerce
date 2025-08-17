6a71097a5016366460726c6130750668416d8731
erikstiven <erikquisnia@gmail.com>
2025-08-17 17:24:12 -0500

    cambios

---
+++ app/Models/Order.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use App\Enums\OrderStatus;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    use HasFactory;
+
+    protected $guarded = [
+        'status'
+    ];
+
+    protected $casts = [
+        'contect' => 'array',
+        'address' => 'array',
+        'status' => OrderStatus::class
+    ];
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+
+
+
+}
---
+++ database/migrations/2025_07_28_164241_create_orders_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+
+            // Relación con usuario
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+
+            // Ruta del PDF generado
+            $table->string('pdf_path')->nullable();
+
+            // Contenido del carrito o productos en formato JSON
+            $table->json('content');
+
+            // Dirección del envío
+            $table->string('address');
+
+            // Método de pago (enum: efectivo o transferencia)
+            $table->enum('payment_method', ['card', 'deposit']);
+
+            // ID de transacción externa (opcional)
+            $table->string('payment_id');
+
+            // Total de la compra
+            $table->decimal('total', 10, 2);
+
+            // Estado de la orden
+            $table->tinyInteger('status')->default(1); // 0: pendiente, 1: pagado, 2: cancelado
+
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
