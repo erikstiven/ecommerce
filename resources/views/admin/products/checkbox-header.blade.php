8f3c8a986daca6defa6ab1fed0c89c0dcf54b6d2
ERIK QUISNIA <105946950+erikstiven@users.noreply.github.com>
2025-12-07 17:42:48 -0500

    Fix updatedSelected signature

--- resources/views/admin/products/checkbox-header.blade.php
+++ resources/views/admin/products/checkbox-header.blade.php
@@ -1,5 +1,5 @@
-<th class="px-3 py-2">
-    <input type="checkbox"
-           wire:click="toggleSelectAll"
-           class="w-4 h-4 rounded border-gray-400 cursor-pointer">
-</th>
+<input
+    type="checkbox"
+    wire:model.live="selectAll"
+    class="w-4 h-4 cursor-pointer"
+    aria-label="Seleccionar todos los productos">
